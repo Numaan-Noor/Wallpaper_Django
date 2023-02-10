928b540d9234f28d10276724f489aab92d160c89
Numaan-Noor <numaannoor81@gmail.com>
2023-02-11 00:08:09 +0530

    changes

--- Wallpaper/settings.py
+++ Wallpaper/settings.py
@@ -78,10 +78,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-        'OPTIONS': {
-            'timeout': 20,  # Timeout value in seconds
-            'readonly': False  # Set to False to allow write access
-        },
+
     }
 }
 
